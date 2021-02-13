37537b5fae248942c6483e36144409aaa9605aaf
hoangunity <hoangdm.141196@gmail.com>
2021-02-13 13:29:58 +0700

    Form Values

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const dateEl = document.getElementById('date-picker');
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+let countdownTitle = '';
+let countdownDate = '';
+
+// Take Values from Form Input
+function updateCountdown(event) {
+  event.preventDefault();
+  countdownTitle = event.srcElement[0].value;
+  countdownDate = event.srcElement[1].value;
+  console.log(countdownTitle, countdownDate);
+}
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
